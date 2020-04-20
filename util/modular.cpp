3ac85ed05ea61f69cf54a9719f4cceee13dce8d9
emorgan00 <emorgan00@stuy.edu>
2020-04-20 10:41:58 -0400

    bug fix in modinv

--- util/modular.cpp
+++ util/modular.cpp
@@ -13,11 +13,12 @@ T powmod(T b, T r, T m) {
 }
 
 // returns a unique number b s.t. ab = 1 mod m
+template<typename T>
 T modinv(T a, T m) {
     T r = 1, k = m-2;
     while (k) {
         if (k&1) r = (r*a)%m;
-        t = (a*a)%m;
+        a = (a*a)%m;
         k >>= 1;
     }
     return r%m;
