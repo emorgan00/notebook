bdf7741352465ad7ee7e486947d46898239e6724
emorgan00 <emorgan00@stuy.edu>
2020-04-20 19:05:59 -0400

    pathmin

---
+++ ds/pathmin.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// unionfind and rmq are used for the pathmin struct below.
+// there are some minor modifications to them, so copy over everything.
+
+struct unionfind {
+
+    int length;
+    vector<int> parent;
+
+    unionfind(int s) {
+        length = s;
+        parent.assign(s, 0);
+        std::iota(parent.begin(), parent.end(), 0);
+    }
+
+    int rep(int x) {
+        while (x != parent[x]) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+
+    void unite(int a, int b) {
+        a = rep(a);
+        b = rep(b);
+        if (a == b)
+            return;
+        parent[b] = a;
+    }
+
+    // returns true if a and b are in the same set.
+    inline bool query(int a, int b) {
+        return rep(a) == rep(b);
+    }
+};
+
+template<typename T>
+struct rmq {
+
+    vector<vector<T>> tree;
+
+    rmq(vector<T>& data) {
+
+        int n = data.size();
+        int log2n = ceil(log2(n));
+        tree.assign(log2n+1, vector<T>(n));
+        tree[0] = data;
+
+        for (int i = 1; i <= log2n; i++)
+            for (int j = 0; j < n-(1<<i)+1; j++)
+                tree[i][j] = min(tree[i-1][j], tree[i-1][j+(1<<i-1)]);
+    }
+
+    T query(int a, int b) {
+        int n = floor(log2(b-a+1));
+        return min(tree[n][a], tree[n][b-(1<<n)+1]);
+    }
+};
+
+struct pathmin {
+
+    int length;
+    unionfind UF;
+    rmq RMQ;
+    vector<int> index;
+
+    // Accepts an adjacency list with pairs in the form {weight, destination}.
+    // Must be a tree. There can only be one edge between two vertices.
+    // O(VlogV) to set up, O(1) to query.
+    pathmin(vector<vector<pair<int, int>>>& adj) {
+
+        length = adj.size();
+        UF = unionfind(length);
+        vector<int> pre(length), post(length), succ(length, -1), weight(length);
+
+        for (int i = 0; i < length; i++)
+            pre[i] = post[i] = i;
+
+        vector<tuple<int, int, int>> edges;
+        edges.reserve(length-1);
+        for (int i = 0; i < length; i++)
+            for (auto p : adj[i])
+                edges.push_back({p.first, i, p.second});
+
+        sort(edges.begin(), edges.end(), greater<tuple<int, int, int>>());
+        for (auto edge : edges) {
+            int w, a, b;
+            tie(w, a, b) = edge;
+            if (UF.query(a, b))
+                continue;
+
+            int Arep = UF.rep(a), Brep = UF.rep(b);
+
+            weight[post[Arep]] = w;
+            succ[post[Arep]] = pre[Brep];
+            UF.unite(Arep, Brep);
+
+            int rep = UF.rep(Arep);
+            pre[rep] = pre[Arep];
+            post[rep] = post[Brep];
+        }
+
+        index = vector<int>(length);
+        vector<int> values = vector<int>(length);
+        int cur = pre[UF.rep(0)], i = 0;
+        while (cur != -1) {
+            index[cur] = i;
+            values[i] = weight[cur];
+            cur = succ[cur];
+            i++;
+        }
+
+        RMQ = rmq(values);
+    }
+
+    // minimum edge along the path from a to b.
+    int query(int a, int b) {
+        a = index[a]; b = index[b];
+        if (a > b) swap(a, b);
+        return RMQ.query(a, b-1);
+    }
+};
