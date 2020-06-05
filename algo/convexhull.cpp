0271c7dcb7dd92e77dd7798c4459490437929382
emorgan00 <emorgan00@stuy.edu>
2020-06-05 13:52:55 -0400

    convex hull improved

---
+++ algo/convexhull.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// computes the indices of all points which
+// are members of the convex hull in O(nlogn) time.
+
+template<typename T>
+struct pt {
+    T x, y;
+};
+
+template<typename T>
+bool cw(pt<T> a, pt<T> b, pt<T> c) {
+    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) < 0;
+}
+
+template<typename T>
+bool ccw(pt<T> a, pt<T> b, pt<T> c) {
+    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) > 0;
+}
+
+// see https://cp-algorithms.com/geometry/grahams-scan-convex-hull.html
+// (this is basically copied from there)
+
+template<typename T>
+vector<int> convexhull(vector<pt<T>>& p) {
+    vector<int> ord(p.size()); iota(ord.begin(), ord.end(), 0);
+    sort(ord.begin(), ord.end(), [&](int i, int j){return p[i] < p[j];});
+    pt<T> a = p[ord[0]], b = p[ord.back()];
+    vector<int> u = {ord[0]}, l = {ord[0]};
+    for (int j = 1; j < p.size(); j++) {
+        int i = ord[j];
+        if (j == p.size()-1 || cw(a, p[i], b)) {
+            while (u.size() >= 2 && !cw(p[u[u.size()-2]], p[u[u.size()-1]], p[i]))
+                u.pop_back();
+            u.push_back(i);
+        }
+        if (j == p.size()-1 || ccw(a, p[i], b)) {
+            while(l.size() >= 2 && !ccw(p[l[l.size()-2]], p[l[l.size()-1]], p[i]))
+                l.pop_back();
+            l.push_back(i);
+        }
+    }
+    for (int i = l.size()-2; i > 0; i--)
+        u.push_back(l[i]);
+    return u;
+}
--- util/geometry.cpp
+++ util/geometry.cpp
@@ -1,55 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef struct point {
-	double x, y;
-	double arg() { return atan2(y, x); }
-	double mod() { return hypot(y, x); }
-} point;
+template<typename T>
+struct pt {
+    T x, y;
+};
 
-typedef struct segment {
-	point a, b;
-} segment;
-
-// short functions
-
-inline double dot(point a, point b) { return a.x*b.x + a.y*b.y; }
-inline double cross(point a, point b) { return a.x*b.y - a.y*b.x; }
-inline double dist(point a, point b) { return hypot(a.x-b.x, a.y-b.y); }
-
-inline bool anglecmp(point a, point b) { return atan2(a.y, a.x) < atan2(b.y, b.x); }
-inline bool distancecmp(point a, point b) { return hypot(a.y, a.x) < hypot(b.y, b.x); }
-
-inline point intersection(point a, point b, point c, point d) {
-	float t = ((a.x-c.x) * (c.y-d.y) - (a.y-c.y) * (c.x-d.x)) / ((a.x-b.x) * (c.y-d.y) - (a.y-b.y) * (c.x-d.x));
-	return {a.x + t*(b.x-a.x), a.y + t*(b.y-a.y)};
-}
-inline point intersection(segment& a, segment& b) { return intersection(a.a, a.b, b.a, b.b); }
-
-inline bool intersects(point a, point b, point c, point d) {
-	float t = ((a.x-c.x) * (c.y-d.y) - (a.y-c.y) * (c.x-d.x)) / ((a.x-b.x) * (c.y-d.y) - (a.y-b.y) * (c.x-d.x));
-	float u = -1 * ((a.x-b.x) * (a.y-c.y) - (a.y-b.y) * (a.x-c.x)) / ((a.x-b.x) * (c.y-d.y) - (a.y-b.y) * (c.x-d.x));
-	return t >= 0 && t <= 1 && u >= 0 && u <= 1;
-}
-inline bool intersects(segment& a, segment& b) { return intersects(a.a, a.b, b.a, b.b); }
-
-// longer, more specialized functions
-
-double area(vector<point> v) { // v.size() must be at least 3.
-	double out = cross(v[v.size()-1], v[0]);
-	for (int i = 0; i < v.size()-1; i++)
-		out += cross(v[i], v[i+1]);
-	return abs(out);
+template<typename T>
+T dot(pt<T> a, pt<T> b) {
+	return a.x*b.x + a.y*b.y;
 }
 
-// returns true if point a is contained in the polygon
-bool contains(vector<segment> v, point a) {
-	int count = 0;
-	point infinity_point = {0, 1e100};
-	for (segment s : v) {
-		segment ray = {a, infinity_point};
-		if (intersects(s, ray));
-			count++; 
-	}
-	return count%2 == 1;
+// returns the point of intersection of lines AB and CD.
+// only works for floats/doubles
+template<typename T>
+pt<T> intersection(pt<T> a, pt<T> b, pt<T> c, pt<T> d) {
+    T t = ((a.x-c.x)*(c.y-d.y)-(a.y-c.y)*(c.x-d.x))/((a.x-b.x)*(c.y-d.y)-(a.y-b.y)*(c.x-d.x));
+    return {a.x + t*(b.x-a.x), a.y + t*(b.y-a.y)};
 }
