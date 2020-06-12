35b324f21a2ab0ef4e0a9df4c47fed16fd5c0ff0
emorgan00 <emorgan00@stuy.edu>
2020-06-11 20:12:49 -0400

    faster coord compression

--- util/coordcompression.cpp
+++ util/coordcompression.cpp
@@ -5,41 +5,30 @@ using namespace std;
 // while preserving the ordering/equality relationship among indices into A.
 // Runs in O(nlogn).
 
-template<typename T>
-vector<int> compress(vector<T>& A) {
-
-	vector<pair<T, int>> ordering(A.size());
-	vector<int> out(A.size());
-	for (int i = 0; i < A.size(); i++)
-		ordering[i] = {A[i], i};
-
-	sort(ordering.begin(), ordering.end());
-
-	int offset = 0;
-	for (int i = 0; i < A.size(); i++) {
-		if (i > 0 && ordering[i].first == ordering[i-1].first) offset++;
-		out[ordering[i].second] = i-offset;
-	}
-	return out;
+template<typename it>
+void compress(it start, it end) {
+    typedef typename remove_reference<decltype(*start)>::type T;
+    map<T, vector<it>> m;
+    for (it i = start; i != end; i++)
+        m[*i].push_back(i);
+    int t = 0;
+    for (auto& [x, v] : m) {
+        for (auto& i : v)
+            *i = t;
+        t++;
+    }
 }
 
-// Same thing, but you can supply a custom comparator to use for A,
-// as well as a function to test if two elements are equivalent
-
-template<typename T, typename X, typename Y>
-vector<int> compress(vector<T>& A, X comp, Y equiv) {
-
-    vector<pair<T, int>> ordering(A.size());
-    vector<int> out(A.size());
-    for (int i = 0; i < A.size(); i++)
-        ordering[i] = {A[i], i};
-
-    sort(ordering.begin(), ordering.end(), [&](pair<T, int>& x, pair<T, int>& y){return comp(x.first, y.first);});
-
-    int offset = 0;
-    for (int i = 0; i < A.size(); i++) {
-        if (i > 0 && equiv(ordering[i].first, ordering[i-1].first)) offset++;
-        out[ordering[i].second] = i-offset;
+template<typename it, typename cmp>
+void compress(it start, it end, cmp ord) {
+    typedef typename remove_reference<decltype(*start)>::type T;
+    map<T, vector<it>, cmp> m(ord);
+    for (it i = start; i != end; i++)
+        m[*i].push_back(i);
+    int t = 0;
+    for (auto& [x, v] : m) {
+        for (auto& i : v)
+            *i = t;
+        t++;
     }
-    return out;
 }
