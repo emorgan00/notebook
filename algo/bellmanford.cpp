0ddb83285701ca791c246dbc44b616b21d415428
emorgan00 <emorgan00@stuy.edu>
2020-07-16 14:51:44 -0400

    clean up bellman ford and topological sort code

--- algo/bellmanford.cpp
+++ algo/bellmanford.cpp
@@ -11,19 +11,19 @@ template<typename T>
 bool negcycle(vector<vector<pair<T, int>>> adj) {
 
     const static T inf_T = numeric_limits<T>::max();
-	int n = adj.size();
-	vector<T> c(n, 0);
+    int n = adj.size();
+    vector<T> c(n, 0);
 
-	bool cyc;
-	for (int v = 0; v < n; v++) {
-		cyc = 0;
-		for (int i = 0; i < n; i++)
-			for (auto [w, j] : adj[i])
-				if (c[i] != inf_T && c[j] > c[i]+w)
-					cyc = 1, c[j] = c[i]+w;
-		if (!cyc) break;
-	}
-	return cyc;
+    bool cyc;
+    for (int v = 0; v < n; v++) {
+        cyc = 0;
+        for (int i = 0; i < n; i++)
+            for (auto [w, j] : adj[i])
+                if (c[i] != inf_T && c[j] > c[i]+w)
+                    cyc = 1, c[j] = c[i]+w;
+        if (!cyc) break;
+    }
+    return cyc;
 }
 
 
@@ -36,59 +36,59 @@ bool negcycle(vector<vector<pair<T, int>>> adj) {
 // Pairs should be in the form {weight, destination}. Runs in O(EV).
 int bellmanford(vector<vector<pair<int, int>>> adj, int start, int end) {
 
-	int size = adj.size();
-	vector<int> cost(size, inf);
-	vector<bool> cycleflag(size, 0), visited(size, 0);
-	cost[start] = 0;
+    int size = adj.size();
+    vector<int> cost(size, inf);
+    vector<bool> cycleflag(size, 0), visited(size, 0);
+    cost[start] = 0;
 
-	for (int v = 1; v < size; v++)
-		for (int i = 0; i < size; i++)
-			for (auto edge : adj[i])
-				if (cost[edge.second] > cost[i]+edge.first && cost[i] != inf)
-					cost[edge.second] = cost[i]+edge.first;
+    for (int v = 1; v < size; v++)
+        for (int i = 0; i < size; i++)
+            for (auto edge : adj[i])
+                if (cost[edge.second] > cost[i]+edge.first && cost[i] != inf)
+                    cost[edge.second] = cost[i]+edge.first;
 
-	bool anycycle = 0;
-	for (int i = 0; i < size; i++)
-		for (auto edge : adj[i])
-			if (cost[edge.second] > cost[i]+edge.first) {
-				cycleflag[edge.second] = 1;
-				anycycle = 1;
-			}
-	if (!anycycle)
-		return cost[end];
+    bool anycycle = 0;
+    for (int i = 0; i < size; i++)
+        for (auto edge : adj[i])
+            if (cost[edge.second] > cost[i]+edge.first) {
+                cycleflag[edge.second] = 1;
+                anycycle = 1;
+            }
+    if (!anycycle)
+        return cost[end];
 
-	vector<int> stack;
-	visited[start] = 1;
-	stack.push_back(start);
-	while (!stack.empty()) {
-		int i = stack.back();
-		stack.pop_back();
-		for (auto edge : adj[i])
-			if (!visited[edge.second]) {
-				visited[edge.second] = 1;
-				stack.push_back(edge.second);
-			}
-	}
+    vector<int> stack;
+    visited[start] = 1;
+    stack.push_back(start);
+    while (!stack.empty()) {
+        int i = stack.back();
+        stack.pop_back();
+        for (auto edge : adj[i])
+            if (!visited[edge.second]) {
+                visited[edge.second] = 1;
+                stack.push_back(edge.second);
+            }
+    }
 
-	for (int i = 0; i < size; i++) {
-		if (!cycleflag[i] && visited[i])
-			visited[i] = 0;
-		if (visited[i])
-			stack.push_back(i);
-	}
-	while (!stack.empty()) {
-		int i = stack.back();
-		stack.pop_back();
-		for (auto edge : adj[i])
-			if (!visited[edge.second]) {
-				visited[edge.second] = 1;
-				stack.push_back(edge.second);
-			}
-	}
+    for (int i = 0; i < size; i++) {
+        if (!cycleflag[i] && visited[i])
+            visited[i] = 0;
+        if (visited[i])
+            stack.push_back(i);
+    }
+    while (!stack.empty()) {
+        int i = stack.back();
+        stack.pop_back();
+        for (auto edge : adj[i])
+            if (!visited[edge.second]) {
+                visited[edge.second] = 1;
+                stack.push_back(edge.second);
+            }
+    }
 
-	if (visited[end])
-		return -inf;
-	return cost[end];
+    if (visited[end])
+        return -inf;
+    return cost[end];
 }
 
 
@@ -100,22 +100,22 @@ int bellmanford(vector<vector<pair<int, int>>> adj, int start, int end) {
 // Pairs should be in the form {weight, destination}. Runs in O(EV).
 vector<int> bellmanford(vector<vector<pair<int, int>>> adj, int start) {
 
-	int size = adj.size();
-	vector<int> cost(size, inf);
-	cost[start] = 0;
+    int size = adj.size();
+    vector<int> cost(size, inf);
+    cost[start] = 0;
 
-	bool cycle;
-	for (int v = 0; v < size; v++) {
-		cycle = 0;
-		for (int i = 0; i < size; i++)
-			for (auto edge : adj[i])
-				if (cost[edge.second] > cost[i]+edge.first && cost[i] != inf) {
-					cycle = 1;
-					cost[edge.second] = cost[i]+edge.first;
-				}
-		if (!cycle) break;
-	}
-	if (cycle)
-		cost.clear();
-	return cost;
+    bool cycle;
+    for (int v = 0; v < size; v++) {
+        cycle = 0;
+        for (int i = 0; i < size; i++)
+            for (auto edge : adj[i])
+                if (cost[edge.second] > cost[i]+edge.first && cost[i] != inf) {
+                    cycle = 1;
+                    cost[edge.second] = cost[i]+edge.first;
+                }
+        if (!cycle) break;
+    }
+    if (cycle)
+        cost.clear();
+    return cost;
 }
--- algo/topologicalsort.cpp
+++ algo/topologicalsort.cpp
@@ -6,26 +6,24 @@ using namespace std;
 
 vector<int> topologicalsort(vector<vector<int>>& adj) {
 
-	int size = adj.size();
-	vector<int> out, stk, in(size, 0);
-	vector<bool> visited(size, 0);
+    int n = adj.size();
+    vector<int> out, stk, in(n, 0);
+    vector<bool> visited(n, 0);
 
-	for (int i = 0; i < size; i++)
-		for (int j : adj[i])
-			in[j]++;
-	for (int i = 0; i < size; i++)
-		if (in[i] == 0)
-			stk.push_back(i);
+    for (int i = 0; i < n; i++)
+        for (int j : adj[i])
+            in[j]++;
+    for (int i = 0; i < n; i++)
+        if (in[i] == 0)
+            stk.push_back(i);
 
-	while (!stk.empty()) {
-		int i = stk.back();
-		stk.pop_back();
-		out.push_back(i);
-		for (int j : adj[i]) {
-			in[j]--;
-			if (in[j] == 0)
-				stk.push_back(j);
-		}
-	}
-	return out;
+    while (!stk.empty()) {
+        int i = stk.back();
+        stk.pop_back();
+        out.push_back(i);
+        for (int j : adj[i])
+            if (--in[j] == 0)
+                stk.push_back(j);
+    }
+    return out;
 }
