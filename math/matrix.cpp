253c39fe69a138ef5a3dd57ffe34df0a5eb880e7
emorgan00 <emorgan00@stuy.edu>
2020-07-19 11:48:14 -0400

    significantly shortened matrix code

--- math/matrix.cpp
+++ math/matrix.cpp
@@ -5,148 +5,49 @@ template<typename T, int H, int W>
 struct matrix {
 
     T M[H][W];
-
-    matrix() {
-        clear();
+    matrix() { clear(); }
+    matrix(bool b) { b ? ident() : clear(); }
+    matrix(vector<vector<T>> v) : matrix() {
+        for (int i = 0; i < v.size(); i++) for (int j = 0; j < v[i].size(); j++)
+            M[i][j] = v[i][j];
     }
 
-    inline int width() const {
-        return W;
-    }
-    inline int height() const {
-        return H;
-    }
+    T* operator[](const int i) { return M[i]; }
+    const T* operator[](const int i) const { return M[i]; }
 
-    // overloaded 2D indexing
-    inline T* operator[](const int i) {
-        return M[i];
-    }
-    inline const T* operator[](const int i) const {
-        return M[i];
+    void clear() { fill(&M[0][0], &M[0][0]+sizeof(M)/sizeof(T), 0); }
+    void ident() {
+        static_assert(H == W, "ident() on non-square matrix"); clear();
+        for (int i = 0; i < W; i++) M[i][i] = 1;
     }
 
-    // reset this matrix to the ZERO MATRIX
-    inline void clear() {
-        std::fill(&M[0][0], &M[0][0] + sizeof(M)/sizeof(T), 0.0);
+    matrix<T, H, W>& operator+=(const matrix<T, H, W>& a) {
+        for (int i = 0; i < H; i++) for (int j = 0; j < W; j++)
+            M[i][j] += a.M[i][j];
+        return *this;
+    }
+    friend matrix<T, H, W> operator+(const matrix<T, H, W>& a, const matrix<T, H, W>& b) {
+        return matrix(a) += b;
+    }
+    template<int C>
+    friend matrix<T, H, W> operator*(const matrix<T, H, C>& a, const matrix<T, C, W>& b) {
+        matrix<T, H, W> r(0);
+        for (int i = 0; i < H; i++) for (int j = 0; j < W; j++) for (int k = 0; k < C; k++)
+            r.M[i][j] += a.M[i][k]*b.M[k][j];
+        return r;
     }
 
-    // reset this matrix to the IDENTITY MATRIX
-    inline void ident() {
-        static_assert(H == W, "A matrix must be square to be reset.");
-        std::fill(&M[0][0], &M[0][0] + sizeof(M)/sizeof(T), 0.0);
-        for (int i = 0; i < W; i++)
-            M[i][i] = 1.0;
-    }
-
-    // equivalent to this = A+this (note that A must be H by W)
-    void add(const matrix<T, H, W>& A) {
-        for (int i = 0; i < H; i++)
-            for (int j = 0; j < W; j++)
-                M[i][j] += A.M[i][j];
-    }
-
-    // equivalent to this = A*this (note that A must be H by H)
-    void multiply_left(const matrix<T, H, H>& A) {
-        int temp[H];
-
-        for (int j = 0; j < W; j++) {
-            std::fill(temp, temp+H, 0.0);
-            for (int i = 0; i < H; i++)
-                for (int k = 0; k < H; k++)
-                    temp[i] += A.M[i][k]*M[k][j];
-            for (int i = 0; i < H; i++)
-                M[i][j] = temp[i];
-        }
-    }
-
-    // equivalent to this = this*A (note that A must be W by W)
-    void multiply_right(const matrix<T, W, W>& A) {
-        int temp[W];
-
+    friend string to_string(const matrix<T, H, W>& a) {
+        string out = "";
         for (int i = 0; i < H; i++) {
-            std::fill(temp, temp+W, 0.0);
+            out += (i == 0 ? "[" : ", ");
             for (int j = 0; j < W; j++)
-                for (int k = 0; k < W; k++)
-                    temp[j] += M[i][k]*A.M[k][j];
-            for (int j = 0; j < W; j++)
-                M[i][j] = temp[j];
+                out += (j == 0 ? "[" : ", ")+to_string(a.M[i][j]);
+            out += "]";
         }
+        return out+"]";
+    }
+    friend ostream& operator<<(ostream& out, const matrix<T, H, W>& a) {
+        return out << to_string(a);
     }
 };
-
-// to_string
-
-template<typename T, int h, int w>
-string to_string(const matrix<T, h, w>& A) {
-    string out = "";
-    for (int i = 0; i < h; i++) {
-        out += (i == 0 ? "[" : ", ");
-        for (int j = 0; j < w; j++) {
-            out += (j == 0 ? "[" : ", ");
-            out += to_string(A[i][j]).substr(0, 6);
-        }
-        out += "]";
-    }
-    out += "]";
-    return out;
-}
-
-template<typename T, int h, int w>
-inline ostream& operator<<(ostream& out, const matrix<T, h, w>& a) {
-    out << to_string(a);
-    return out;
-}
-
-// matrix arithmetic
-
-template<typename T, int ah, int aw, int bh, int bw>
-matrix<T, ah, aw> operator+(const matrix<T, ah, aw>& A, const matrix<T, bh, bw>& B) {
-
-    static_assert(aw == bw && ah == bh, "A and B must have the same dimensions.");
-    matrix<T, ah, aw> R;
-
-    for (int i = 0; i < ah; i++)
-        for (int j = 0; j < bw; j++)
-            R.M[i][j] = A.M[i][j]+B.M[i][j];
-
-    return R;
-}
-
-template<typename T, int ah, int aw, int bh, int bw>
-matrix<T, ah, bw> operator*(const matrix<T, ah, aw>& A, const matrix<T, bh, bw>& B) {
-
-    static_assert(aw == bh, "A.width() must equal B.height().");
-    matrix<T, ah, bw> R;
-    R.clear();
-
-    for (int i = 0; i < ah; i++)
-        for (int j = 0; j < bw; j++)
-            for (int k = 0; k < aw; k++)
-                R.M[i][j] += A.M[i][k]*B.M[k][j];
-
-    return R;
-}
-
-template<typename T, int ah, int aw, int bh, int bw>
-inline void operator+=(matrix<T, ah, aw>& A, const matrix<T, bh, bw>& B) {
-    static_assert(aw == bw && ah == bh, "A and B must have the same dimensions.");
-    A.add(B);
-}
-
-// zero matrix (example use: auto M = zero<int, 5, 5>();)
-
-template<typename T, int h, int w>
-inline constexpr matrix<T, h, w> zero() {
-    matrix<T, h, w> M;
-    M.clear();
-    return M;
-}
-
-// identity matrix (example use: auto M = identity<int, 5, 5>();)
-
-template<typename T, int h, int w>
-inline constexpr matrix<T, h, w> identity() {
-    matrix<T, h, w> M;
-    M.ident();
-    return M;
-}
