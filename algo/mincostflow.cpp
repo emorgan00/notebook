8dc1eb4b535efa58b754db2dbee0b2ced5c9eb94
emorgan00 <emorgan00@stuy.edu>
2020-10-24 16:21:25 -0400

    forgot to make latest changes

--- algo/mincostflow.cpp
+++ algo/mincostflow.cpp
@@ -16,38 +16,38 @@ struct mincostflow {
     }
 
     bool spfa() {
-        fill(l, l+N, inf_T), l[s] = 0, pv[s] = -1;
+        fill(l, l+N, inf_T), l[s] = 0;
         queue<int> q({s});
         while (!q.empty()) {
             int v = q.front(); q.pop();
             vis[v] = 0;
-            for (int i = 0; i < adj[v].size(); i++) {
-                auto& e = adj[v][i];
+            for (auto& e : adj[v])
                 if (l[v] != inf_T && e.f < e.w && l[v]+e.c < l[e.v]) {
-                    l[e.v] = l[v]+e.c, pv[e.v] = v, pu[e.v] = i;
+                    l[e.v] = l[v]+e.c, p[e.v] = e.u;
                     if (!vis[e.v]) q.push(e.v), vis[e.v] = 1;
                 }
-            }
         }
         return l[t] != inf_T;
     }
 
     // compute the maximum flow from s to t, and the minimum cost needed to do it, one-time use.
-    // SPFA + Ford, complexity is O(?????)
+    // SPFA + Edmonds-Karp, complexity is O(?????)
     // negative cost cycles in the original graph break it
     pair<T, T> solve(int _s, int _t) {
         s = _s, t = _t; T f = 0, c = 0;
         while (spfa()) {
             T x = inf_T;
-            for (int v = t; v != s; v = adj[v][p[v]].v)
-                x = min(x, adj[v][p[v]].w-adj[v][p[v]].f);
             for (int v = t; v != s; v = adj[v][p[v]].v) {
                 auto& e = adj[v][p[v]];
-                c += e.c*x, e.f -= x, adj[e.v][e.u].f += x;
+                x = min(x, adj[e.v][e.u].w-adj[e.v][e.u].f);
+            }
+            for (int v = t; v != s; v = adj[v][p[v]].v) {
+                auto& e = adj[v][p[v]];
+                c -= e.c*x, e.f -= x, adj[e.v][e.u].f += x;
             }
             f += x;
         }
-        return {f, c/2};
+        return {f, c};
     }
 
     // after calling solve(), return a component vector C s.t. C[i] \in {0, 1}
