fda464f9da11667adf442d99e74fbd57a30697f4
emorgan00 <emorgan00@stuy.edu>
2020-10-24 17:56:37 -0400

    dijkstra's with johnson potentials for mcmf

--- algo/mincostflow.cpp
+++ algo/mincostflow.cpp
@@ -2,17 +2,28 @@
 template<int N, typename T>
 struct mincostflow {
 
-    const T inf_T = numeric_limits<T>::max();
+    const T inf_T = numeric_limits<T>::max()/100000;
+    const bool CYCLIC = true; // false will usually make it run faster, but requires a DAG
 
     struct flow_edge { int v, u; T f, w, c; };
     vector<flow_edge> adj[N];
-    T l[N]; int p[N], vis[N], s, t;
+    T l[N], pot[N];
+    int p[N], vis[N], s, t;
+    priority_queue<pair<T, int>, vector<pair<T, int>>, greater<>> pq;
 
     // add an edge from i to j with capacity w and cost per unit flow c
     void edge(int i, int j, T w, T c, bool directed = true) {
         adj[i].push_back({j, int(adj[j].size()), 0, w, c});
         adj[j].push_back({i, int(adj[i].size())-1, 0, 0, -c});
         if (!directed) edge(j, i, w, c);
+    }
+
+    T dp(int v) {
+        if (vis[v] || v == s) return pot[v];
+        for (auto& e : adj[v])
+            if (adj[e.v][e.u].w != 0)
+                pot[v] = min(pot[v], dp(e.v)-e.c);
+        return vis[v] = 1, pot[v]; 
     }
 
     bool spfa() {
@@ -30,12 +41,31 @@ struct mincostflow {
         return l[t] != inf_T;
     }
 
+    bool dij() {
+        fill(l, l+N, inf_T), pq.push({l[s] = 0, s});
+        while (!pq.empty()) {
+            auto [c, v] = pq.top(); pq.pop();
+            if (l[v] < c) continue;
+            for (auto& e : adj[v]) {
+                T d = e.c+pot[v]-pot[e.v];
+                if (l[v] != inf_T && e.f < e.w && c+d < l[e.v])
+                    p[e.v] = e.u, pq.push({l[e.v] = c+d, e.v});
+            }
+        }
+        for (int i = 0; i < N; i++)
+            if (l[i] != inf_T) pot[i] += l[i];
+        return l[t] != inf_T;
+    }
+
     // compute the maximum flow from s to t, and the minimum cost needed to do it, one-time use.
-    // SPFA + Edmonds-Karp, complexity is O(?????)
-    // negative cost cycles in the original graph break it
+    // for cyclic graphs: SPFA + Edmonds-Karp, complexity is O(?????)
+    // for DAGS: Dijkstra's w/ Johnson potentials + Edmonds-Karp, complexity is O(?????)
+    // negative weight cycles will always break it
     pair<T, T> solve(int _s, int _t) {
         s = _s, t = _t; T f = 0, c = 0;
-        while (spfa()) {
+        fill(pot, pot+N, inf_T), pot[s] = 0;
+        if (!CYCLIC) dp(t);
+        while (CYCLIC ? spfa() : dij()) {
             T x = inf_T;
             for (int v = t; v != s; v = adj[v][p[v]].v) {
                 auto& e = adj[v][p[v]];
