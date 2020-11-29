ebeb0fdf99cfcfa0b6c33566a84f9a882e8e0a51
emorgan00 <emorgan00@stuy.edu>
2020-11-28 22:46:22 -0500

    coord compression one function

--- util/coordcompression.cpp
+++ util/coordcompression.cpp
@@ -1,23 +1,10 @@
 // Transform the contents of [first, last) into elements of [0..N],
-// while preserving the ordering/equality relationship among indices into the data.
+// while preserving the relative ordering/equality of the data.
 // Runs in O(nlogn).
 
-template<typename it1, typename it2>
-void compress(it1 first, it1 last, it2 result) {
-    typedef typename remove_reference<decltype(*first)>::type T;
-    map<T, vector<it2>> m;
-    for (it1 i = first; i != last; i++)
-        m[*i].push_back(result+(i-first));
-    int t = 0;
-    for (auto& [x, v] : m) {
-        for (auto& i : v) *i = t;
-        t++;
-    }
-}
-
-template<typename it1, typename it2, typename cmp>
-void compress(it1 first, it1 last, it2 result, cmp ord) {
-    typedef typename remove_reference<decltype(*first)>::type T;
+template<typename it1, typename it2, typename cmp = less<>>
+void compress(it1 first, it1 last, it2 result, cmp ord = less<>()) {
+    using T = typename remove_reference<decltype(*first)>::type;
     map<T, vector<it2>, cmp> m(ord);
     for (it1 i = first; i != last; i++)
         m[*i].push_back(result+(i-first));
