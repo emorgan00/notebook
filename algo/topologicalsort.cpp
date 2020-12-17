594ca3fa5962c431ce68a2e1825f70cbd28af12b
emorgan00 <emorgan00@stuy.edu>
2020-12-17 00:21:50 -0500

    fix toposort

--- algo/topologicalsort.cpp
+++ algo/topologicalsort.cpp
@@ -3,13 +3,13 @@
 
 vector<int> toposort(vector<vector<int>> adj) {
     vector<int> out, stk, in(adj.size(), 0);
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < adj.size(); i++)
         for (int j : adj[i]) in[j]++;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < adj.size(); i++)
         if (!in[i]) stk.push_back(i);
     while (!stk.empty()) {
-        int i = stk.back(); stk.pop_back();
-        for (int j : adj[i])
+        out.pb(stk.back()); stk.pop_back();
+        for (int j : adj[out.back()])
             if (!--in[j]) stk.push_back(j);
     }
     return out;
