065dfa3ab20b22e9b00bc0de18163d219f3f9560
emorgan00 <emorgan00@stuy.edu>
2020-12-17 01:22:02 -0500

    shorter fixedrangermq

--- algo/fixedrangermq.cpp
+++ algo/fixedrangermq.cpp
@@ -1,23 +1,14 @@
 // Returns a vector V where V[i] = minimum of the array from i to i+width-1.
 // Runs in O(n) time.
 
-template<typename T>
-vector<T> fixedrangermq(vector<T>& A, int width) {
-
-    int n = A.size();
-    vector<T> prefix(n, 0), suffix(n, 0);
-    for (int i = 0; i < n; i++) {
-        prefix[i] = A[i];
-        if (i%width != 0)
-            prefix[i] = min(prefix[i], prefix[i-1]);
-    }
-    for (int i = n-1; i >= 0; i--) {
-        suffix[i] = A[i];
-        if (i%width != width-1 && i != n-1)
-            suffix[i] = min(suffix[i], suffix[i+1]);
-    }
-    vector<T> out(n-width+1);
-    for (int i = 0; i <= n-width; i++)
-        out[i] = min(suffix[i], prefix[i+width-1]);
-    return out;
+template<typename it1, typename it2>
+void fixedrangermq(it1 first, it1 last, it2 result, int width) {
+    int n = distance(first, last);
+    vector p(first, last), s(first, last);
+    for (int i = n-1; i >= 0; i--)
+        if (i%width != width-1 && i != n-1) s[i] = min(s[i], s[i+1]);
+    for (int i = 0; i < n; i++)
+        if (i%width) p[i] = min(p[i], p[i-1]);
+    for (int i = 0; i <= n-width; i++, result++)
+        *result = min(s[i], p[i+width-1]);
 }
