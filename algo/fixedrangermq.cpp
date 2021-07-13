2fb2d4f690a16e8453814d5c9d5e95b7af585555
emorgan00 <emorgan00@stuy.edu>
2021-07-13 10:09:00 -0400

    sliding_window_fold with edges

--- algo/fixedrangermq.cpp
+++ algo/fixedrangermq.cpp
@@ -1,17 +1,5 @@
-// result[i] = minimum of the array from i to i+width-1, runs in O(n) time.
-
-template<typename it1, typename it2>
-void fixedrangermq(it1 first, it1 last, it2 result, int width) {
-    int n = distance(first, last);
-    vector p(first, last), s(first, last);
-    for (int i = n-1; i >= 0; i--)
-        if (i%width != width-1 && i != n-1) s[i] = min(s[i], s[i+1]);
-    for (int i = 0; i < n; i++)
-        if (i%width) p[i] = min(p[i], p[i-1]);
-    for (int i = 0; i <= n-width; i++, result++)
-        *result = min(s[i], p[i+width-1]);
-}
-
+// given an array a of length n, returns an array b of length n-k+1
+// such that b[i] = op(a[i], a[i+1], ..., a[i+k-1])
 template<typename it1, typename it2, typename bin_op>
 void sliding_window_fold(it1 first, it1 last, it2 result, int width, bin_op op) {
     int n = distance(first, last);
@@ -22,4 +10,23 @@ void sliding_window_fold(it1 first, it1 last, it2 result, int width, bin_op op) 
         if (i%width) p[i] = op(p[i], p[i-1]);
     for (int i = 0; i <= n-width; i++, result++)
         *result = i%width ? op(s[i], p[i+width-1]) : s[i];
+}
+
+// given an array a of length n, returns an array b of length n+k-1
+// such that b[i] = op(a[i], a[i+1], ..., a[i+k-1]), not including invalid indices
+template<typename it1, typename it2, typename bin_op>
+void sliding_window_fold2(it1 first, it1 last, it2 result, int width, bin_op op) {
+    int n = distance(first, last);
+    vector p(first, last), s(first, last);
+    for (int i = n-1; i >= 0; i--)
+        if (i%width != width-1 && i != n-1) s[i] = op(s[i], s[i+1]);
+    for (int i = 0; i < n; i++)
+        if (i%width) p[i] = op(p[i], p[i-1]);
+    for (int i = 0; i < width-1; i++, result++)
+        *result = p[i];
+    for (int i = 0; i <= n-width; i++, result++)
+        *result = i%width ? op(s[i], p[i+width-1]) : s[i];
+    int b = (n/width)*width;
+    for (int i = n-width+1; i < n; i++, result++)
+        *result = n%width && i < b ? op(s[i], p[n-1]) : s[i];
 }
