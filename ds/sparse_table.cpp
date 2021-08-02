58ce85ab9609936f226adb43dc28ddd062d71ecd
emorgan00 <emorgan00@stuy.edu>
2021-08-02 18:13:10 -0500

    remove t(1) from sparse_table

--- ds/sparse_table.cpp
+++ ds/sparse_table.cpp
@@ -4,7 +4,7 @@ struct sparse_table {
     using T = typename remove_reference<decltype(*declval<it>())>::type;
     vector<vector<T>> t; bin_op f;
 
-    sparse_table(it first, it last, bin_op op) : t(1), f(op) {
+    sparse_table(it first, it last, bin_op op) : f(op) {
         int n = distance(first, last);
         t.assign(32-__builtin_clz(n), vector<T>(n));
         t[0].assign(first, last);
