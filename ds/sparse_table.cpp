679bf65b7460aa2f3fa4bda98edd52963989e8a6
emorgan00 <emorgan00@stuy.edu>
2021-10-16 10:43:44 -0500

    sparse_table change

--- ds/sparse_table.cpp
+++ ds/sparse_table.cpp
@@ -4,13 +4,18 @@ struct sparse_table {
     using T = typename iterator_traits<it>::value_type;
     vector<vector<T>> t; bin_op f;
 
-    sparse_table(it first, it last, bin_op op) : f(op) {
+    void assign(it first, it last) {
         int n = distance(first, last);
         t.assign(32-__builtin_clz(n), vector<T>(n));
         t[0].assign(first, last);
         for (int i = 1; i < t.size(); i++)
             for (int j = 0; j < n-(1<<i)+1; j++)
                 t[i][j] = f(t[i-1][j], t[i-1][j+(1<<(i-1))]);
+    }
+
+    sparse_table(bin_op op) : f(op) {}
+    sparse_table(it first, it last, bin_op op) : f(op) {
+        assign(first, last);
     }
 
     // returns f(a[l..r]) in O(1) time
